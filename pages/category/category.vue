a176e0f871067050042b594979728780669b5a1f
xatongxue <1120877019@qq.com>
2020-08-21 13:38:30 +0800

    分类样式调试

--- pages/category/category.vue
+++ pages/category/category.vue
@@ -1,18 +1,18 @@
 <template>
 	<div class="category">
 		<view class="list">
-			<view v-for="(item,index) in categoriesList" :key="item.car_id">
-				<text :class="{active:activeIndex===index}" @click="carClick(index)">{{item.cat_name}}</text>
+			<view v-for="(item,index) in categoriesList" :key="item.car_id" :class="{active:activeIndex===index}">
+				<text @click="carClick(index)">{{item.cat_name}}</text>
 			</view>
 		</view>
 		<view class="details">
 			<image class="title-img" src="../../static/images/titleImage.png" mode=""></image>
-			<view v-for="(item,index) in categoriesList[activeIndex].children" :key="cat_id">
-				<view class="details-title">
+			<view  v-for="(item,index) in categoriesList[activeIndex].children" :key="cat_id">
+				<view class="details-title" v-if="item.children" >
 					<text class="cat-name">{{item.cat_name}}</text>
 				</view>
 				<view class="details-item">
-					<view v-for="(item2,index2) in item.children" :key="cat_id">
+					<view  v-for="(item2,index2) in item.children" :key="cat_id">
 						<image :src="item2.cat_icon" mode=""></image>
 						<text>{{item2.cat_name}}</text>
 					</view>
@@ -49,22 +49,28 @@
 <style lang="less">
 	.category {
 		display: flex;
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
 
 		.list {
 			width: 200rpx;
 			background-color: #f5f5f5;
 			color: #434345;
+			overflow: scroll;
 
 			view {
+				display: flex;
+				align-items: center;
 				text-align: center;
 				height: 120rpx;
 
 				text {
-					display: inline-block;
 					width: 200rpx;
-					height: 100rpx;
-					line-height: 100rpx;
-					box-sizing: border-box;
+					height: 70rpx;
+					line-height: 70rpx;
 				}
 			}
 
@@ -72,22 +78,29 @@
 				background-color: #FFFFFF;
 				color: #ff2d4a;
 				font-weight: 700;
-				border-left: 6rpx solid #ff3954;
+
+				text {
+					border-left: 6rpx solid #ff3954;
+				}
 			}
 		}
 
 		.details {
 			flex: 1;
 			color: #434345;
-.title-img{
-	height: 200rpx;
-	width: 93%;
-	margin: 20rpx;
-}
+			overflow: scroll;
+
+			.title-img {
+				height: 200rpx;
+				width: 93%;
+				margin: 20rpx;
+			}
+
 			.details-title {
 				width: 100%;
 				text-align: center;
 				margin: 40rpx 0;
+
 				&::before {
 					content: "/　　　";
 					color: #e3e3e3;
